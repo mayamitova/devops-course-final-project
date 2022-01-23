a58791b5859d40595ee21e8f3c2a89ebf6914f50
Maya Mitova <maya.mitova@taulia.com>
2022-01-23 16:50:00 +0200

    Added V2_insert_courses.sql

---
+++ src/main/resources/db/migration/mysql/V2__insert_courses.sql
@@ -0,0 +1,4 @@
+insert into course (title, presenter) values ('Upskill DevOps', 'Daniel Rankov');
+insert into course (title, presenter) values ('Java', 'Ivan Ivanov');
+insert into course (title, presenter) values ('C#', 'Petar Petrov');
+insert into course (title, presenter) values ('HTML & CSS', 'Todor Todorov');
